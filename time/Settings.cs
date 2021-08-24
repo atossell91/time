a4c75d3d923d32d31c574805ec89f60750f86f6e
atosse,l91 <atossell91@gmail.com>
2021-08-24 12:35:32 -0600

    Added settings class (currently not used)

---
+++ time/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace time
+{
+    class Settings
+    {
+        public bool RoundOT { get; set; } = true;
+        public bool SplitSaturday { get; set; } = true;
+        private Settings() { }
+        public static Settings LoadSettings(string path)
+        {
+
+            if (!File.Exists(path))
+            {
+                return new Settings();
+            }
+
+            string content = File.ReadAllText(path);
+            Settings s = JsonSerializer.Deserialize<Settings>(content);
+            return s;
+        }
+        public void Save(string path)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+            string content = JsonSerializer.Serialize<Settings>(this, options);
+        }
+    }
+}
